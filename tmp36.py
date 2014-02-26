a82d26d2093ff33797a8247ed3a0593eb59acda5
alexander-daniel <alex@lesinstruments.com>
2014-02-25 19:26:28 -0500

    tmp36.py updates

--- tmp36.py
+++ tmp36.py
@@ -78,10 +78,10 @@ LAYOUT = {
     }
 }
 
-# read SPI data from MCP3008 chip, 8 possible adc's (0 thru 7)
+# Function to read data from Analog Pin 0 from MCP3008
 def readadc(adcnum, clockpin, mosipin, misopin, cspin):
-    if ((adcnum > 7) or (adcnum < 0)):
-        return -1
+        if ((adcnum > 7) or (adcnum < 0)):
+                return -1
         GPIO.output(cspin, True)
 
         GPIO.output(clockpin, False)  # start clock low
@@ -91,24 +91,24 @@ def readadc(adcnum, clockpin, mosipin, misopin, cspin):
         commandout |= 0x18  # start bit + single-ended bit
         commandout <<= 3    # we only need to send 5 bits here
         for i in range(5):
-            if (commandout & 0x80):
-                GPIO.output(mosipin, True)
-            else:
-                GPIO.output(mosipin, False)
+                if (commandout & 0x80):
+                        GPIO.output(mosipin, True)
+                else:
+                        GPIO.output(mosipin, False)
                 commandout <<= 1
                 GPIO.output(clockpin, True)
                 GPIO.output(clockpin, False)
 
-                adcout = 0
+        adcout = 0
         # read in one empty bit, one null bit and 10 ADC bits
         for i in range(12):
-            GPIO.output(clockpin, True)
-            GPIO.output(clockpin, False)
-            adcout <<= 1
-            if (GPIO.input(misopin)):
-                adcout |= 0x1
+                GPIO.output(clockpin, True)
+                GPIO.output(clockpin, False)
+                adcout <<= 1
+                if (GPIO.input(misopin)):
+                        adcout |= 0x1
 
-                GPIO.output(cspin, True)
+        GPIO.output(cspin, True)
 
         adcout /= 2       # first bit is 'null' so drop it
         return adcout
@@ -121,12 +121,10 @@ SPIMOSI = 24
 SPICS = 25
 
 # set up the SPI interface pins
-GPIO.setwarnings(False)
 GPIO.setup(SPIMOSI, GPIO.OUT)
 GPIO.setup(SPIMISO, GPIO.IN)
 GPIO.setup(SPICLK, GPIO.OUT)
 GPIO.setup(SPICS, GPIO.OUT)
-
 # temperature sensor connected channel 0 of mcp3008
 adcnum = 0
 
@@ -168,7 +166,8 @@ while True:
             'name' : TEMP_LEGEND,
             'type' : chart_type
             }]
-
+            print temp_C
+            print date_stamp
             response = py.plot(data, filename=FILENAME, fileopt='extend', layout=LAYOUT)
             if response[u'error'] != '' or response[u'message'] != '' or response[u'warning'] != '':
                 quit()
